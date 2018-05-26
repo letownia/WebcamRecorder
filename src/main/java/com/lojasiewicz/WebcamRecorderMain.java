d840781920cd32467cc5af749964eb90c0ea5c44
letownia <borglq@gmail.com>
2018-05-26 14:58:30 +0200

    Initial Commit (will be overwriting github version)

---
+++ src/main/java/com/lojasiewicz/WebcamRecorderMain.java
@@ -0,0 +1,7 @@
+package com.lojasiewicz;
+
+public class WebcamRecorderMain {
+    public static void main(String args[]){
+        System.out.println("Welcome to WebcamRecorder Application");
+    }
+}
